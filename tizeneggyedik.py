9c78f160652e8cf616f1e7c0401fabb5f9880af4
Arcadil <kerenyibalazs12@gmail.com>
2022-12-21 19:17:35 +0100

    Update tizeneggyedik.py

--- tizeneggyedik.py
+++ tizeneggyedik.py
@@ -1,16 +1,30 @@
-class filmek:
+class film:
     def __init__(self, a, b, c, d, e):
         self.cim = a
-        self.mikor = b
+        self.mikor = int(b)
         self.rendezo = c
-        self.cost = d
-        self.income = e
-    def kiirato(self):
-        print(f"{self.cim}\t{self.mikor}\t{self.rendezo}\t{self.cost}\t{self.income}")
+        self.cost = int(d)
+        self.income = int(e)
+#    def kiirato(self):
+#        print(f"{self.cim}\t{self.mikor}\t{self.rendezo}\t{self.cost}\t{self.income}")
 
 adatok = []
 with open("films.txt", "r", encoding="UTF-8") as forrasfajl:
-    for i in forrasfajl:
-        szettordeles = i.strip().split(";")
-        adatok.append(filmek(szettordeles[0],szettordeles[1],szettordeles[2],szettordeles[3],szettordeles[4]))
-print(adatok[0].kiirato())
+    for sor in forrasfajl:
+        szettordeles = sor.strip().split(";")
+        adatok.append(film(szettordeles[0], szettordeles[1], szettordeles[2], szettordeles[3], szettordeles[4]))
+
+
+
+for k in range(len(adatok)-1):
+    if adatok[k].rendezo == "Christopher Nolan":
+        print(adatok[k].cim)
+
+'''
+for a in adatok:
+    if a.rendezo == "Christopher Nolan":
+        print(a.cim)
+'''
+for i in range(len(adatok)-1):
+    if adatok[i].mikor > 1999:
+        print(adatok[i].cim)
